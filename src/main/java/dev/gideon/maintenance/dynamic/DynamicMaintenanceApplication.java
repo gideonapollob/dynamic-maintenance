e66f37467309f89c40046a83f5f5c2c45969102d
gideonapollob <gideonapollob@gmail.com>
2021-01-20 22:57:38 +0800

    Initial commit

---
+++ src/main/java/dev/gideon/maintenance/dynamic/DynamicMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package dev.gideon.maintenance.dynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicMaintenanceApplication.class, args);
+	}
+
+}
